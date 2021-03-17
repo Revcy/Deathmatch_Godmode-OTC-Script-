ec1d769b934aa679bbfaba51a34a3ae7363c2cf7
Semyon Likhorad <63965069+Revcy@users.noreply.github.com>
2021-03-17 21:03:07 +0300

    Add files via upload

---
+++ gm.js
@@ -0,0 +1,24 @@
+UI.AddLabel("Deathmatch Godmode");
+UI.AddLabel("by Revcy, pers0na5, matthewthecoddy (aka matt)");
+
+var lastTick = 0;
+
+function dm_gm() 
+{
+    if (!World.GetServerString())
+        return;
+
+    if (!Entity.IsAlive(Entity.GetLocalPlayer())) 
+        return;
+
+    currentTick = parseInt(Globals.Curtime() * 1000);
+
+    if ((currentTick - 100) >= lastTick) {
+        Cheat.ExecuteCommand( "open_buymenu" );
+        Cheat.ExecuteCommand( "open_buymenu" );
+        lastTick = currentTick;
+    } else if (currentTick < 5000) 
+        lastTick = 0;
+}
+
+Cheat.RegisterCallback("FrameStageNotify", "dm_gm");
