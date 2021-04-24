4546aebbf4b24970da0a16e018db828e8fde00d3
Semyon Likhorad <63965069+Revcy@users.noreply.github.com>
2021-04-24 16:52:01 +0300

    Update gm.js

--- gm.js
+++ gm.js
@@ -2,11 +2,13 @@ UI.AddLabel("Deathmatch Godmode");
 UI.AddLabel("by Revcy, pers0na5, matthewthecoddy (aka matt)");
 
 var lastTick = 0;
+var currentTick = 0;
 
 function dm_gm() 
 {
     if (!World.GetServerString())
         lastTick = 0;
+        currentTick = 0;
         return;
 
     if (!Entity.IsAlive(Entity.GetLocalPlayer())) 
